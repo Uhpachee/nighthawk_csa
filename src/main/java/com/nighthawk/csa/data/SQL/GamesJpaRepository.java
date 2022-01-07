f95caaa1eb0adb575c555d41d99b68bfcd33575a
RohanKoshy <rkosh230@gmail.com>
2022-01-07 10:11:12 -0800

    Database rework

---
+++ src/main/java/com/nighthawk/csa/data/SQL/GamesJpaRepository.java
@@ -0,0 +1,7 @@
+package com.nighthawk.csa.data.SQL;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface GamesJpaRepository extends JpaRepository<Game, Long> {
+    List<Game> RatingandNameofGame(String name, int rating);
+}
