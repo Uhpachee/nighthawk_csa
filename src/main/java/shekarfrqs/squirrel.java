cf1b84c5b5c1b7d4ed475e3c634610b7e27948f3
shekar <shekar.km26@gmail.com>
2022-02-15 11:25:24 -0800

    code thing class frq

--- src/main/java/shekarfrqs/squirrel.java
+++ src/main/java/shekarfrqs/squirrel.java
@@ -8,6 +8,26 @@ public class squirrel {
     String tempString = "";
     Integer listnumber = 0;
 
+    public class Number {
+        Random rand = new Random();
+        int result = rand.nextInt(9);
+    }
+
+    Number squirrel1 = new Number();
+    Number squirrel2 = new Number();
+    Number squirrel3 = new Number();
+    Number squirrel4 = new Number();
+    Number squirrel5 = new Number();
+    Number squirrel6 = new Number();
+    Number squirrel7 = new Number();
+    Number squirrel8 = new Number();
+    Number squirrel9 = new Number();
+    Number squirrel10 = new Number();
+
+
+    Number[] unorderedSquirrels = {squirrel1, squirrel2, squirrel3, squirrel4, squirrel5, squirrel6, squirrel7, squirrel8, squirrel9, squirrel10};
+
+
     public int randnum() {
         Random rand = new Random();
         int result = rand.nextInt(9);
@@ -19,7 +39,7 @@ public class squirrel {
 
 
     public void passGen() {
-        for (int i = 1; i < 8; i++) {
+        for (int i = 1; i < 11; i++) {
             for (int l = 1; l < 3; l++) {
                 tempString += chickennumber.charAt(randnum());
 
